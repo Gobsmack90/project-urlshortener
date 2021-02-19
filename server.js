150da59f0a720029c13e30327396228c86f4d38e
Harrison King <authorharrisonking@gmail.com>
2021-02-18 21:20:07 -0700

    well

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
